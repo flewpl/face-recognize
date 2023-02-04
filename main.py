be5a714539a956b4b21e4d7d68b9dc4aa1ef6c66
flewpl <maxvarusha@gmail.com>
2023-02-04 20:07:27 +0200

    face detection

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import cv2
 import numpy as np
-# photo = np.zeros((500,500,3), dtype='uint8')
-# # photo[:] = 0,255,0
-# photo = cv2.rectangle(photo, (150,150), (350,350),(0,255,255), thickness=2)
-# # photo= cv2.circle(photo, (photo.shape[1]//2,(photo.shape[0]//2)),50, (0,255,0), thickness=1)
-# # photo = cv2.line(photo, (150,150),(350,350),(128, 0, 128), thickness=2 )
-# # photo = cv2.line(photo, (150,350),(350,150),(128, 0, 128), thickness=2 )
-# photo = cv2.putText(photo, 'Me',(photo.shape[1]//2 - 25,photo.shape[0]//2),cv2.FONT_HERSHEY_TRIPLEX,1,(0,255,255),thickness=2)
 
-# cv2.imshow('Photo', photo)
-# cv2.waitKey(0)
+img = cv2.imread('images/images.jpg')
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+faces= cv2.CascadeClassifier('faces.xml')
 
-img = cv2.imread("images/my_photo.png")
+results = faces.detectMultiScale(grey, scaleFactor =1.1, minNeighbors =3 )
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
-
-r,g,b = cv2.split(img)
-
-img = cv2.merge([g])
+for(x,y,w,h) in results:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), thickness=2)
 
 
 
-cv2.imshow('result', img)
+cv2.imshow('Result',img)
 cv2.waitKey(0)
+while True:
+    cap = cv2.VideoCapture("film/video.mp4")
+
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces= cv2.CascadeClassifier('faces.xml')
+
+    results = faces.detectMultiScale(grey, scaleFactor =1.1, minNeighbors =3 )
+    for(x,y,w,h) in results:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), thickness=2)
+    
+
+
+    while True:
+        sucess, img = cap.read()
+
+        cv2.imshow('Result', img)
+
+        if cv2.waitKey(1) & ord('q') == 0xFF:
+            break
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,14 @@
-import cv2
-import numpy as np
-# photo = np.zeros((500,500,3), dtype='uint8')
-# # photo[:] = 0,255,0
-# photo = cv2.rectangle(photo, (150,150), (350,350),(0,255,255), thickness=2)
-# # photo= cv2.circle(photo, (photo.shape[1]//2,(photo.shape[0]//2)),50, (0,255,0), thickness=1)
-# # photo = cv2.line(photo, (150,150),(350,350),(128, 0, 128), thickness=2 )
-# # photo = cv2.line(photo, (150,350),(350,150),(128, 0, 128), thickness=2 )
-# photo = cv2.putText(photo, 'Me',(photo.shape[1]//2 - 25,photo.shape[0]//2),cv2.FONT_HERSHEY_TRIPLEX,1,(0,255,255),thickness=2)
+img = cv2.imread('images/images.jpg')
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# cv2.imshow('Photo', photo)
-# cv2.waitKey(0)
+faces= cv2.CascadeClassifier('faces.xml')
 
-cap = cv2.VideoCapture("film/video.mp4")
-cap.set(3, 500)
-cap.set(4, 300)
+results = faces.detectMultiScale(grey, scaleFactor =1.1, minNeighbors =3 )
 
-while True:
-    success, img = cap.read()
-    cv2.imshow('Result', img)
+for(x,y,w,h) in results:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), thickness=2)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+
+cv2.imshow('Result',img)
+cv2.waitKey(0)
